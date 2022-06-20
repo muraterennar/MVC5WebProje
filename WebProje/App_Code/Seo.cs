cb922e17bfd1501bc3483cd0e3347a8fc82aee45
Murat Eren Nar <muraterennar@hotmail.com>
2022-06-20 22:35:35 +0300

    20.06.2022 Commit1 / Login

---
+++ WebProje/App_Code/Seo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace WebProje.App_Code
+{
+    public class Seo
+    {
+        public static string FolderNameEdit(string folderName)
+        {
+            string NoUrlFolderName = Path.GetFileNameWithoutExtension(folderName);
+            string url = Path.GetExtension(folderName);
+            return UrlEdit(NoUrlFolderName) + url;
+        }
+
+        public static string UrlEdit(object a)
+        {
+            string s = a.ToString();
+            if (string.IsNullOrEmpty(s))
+            {
+                return "";
+            }
+
+            if (s.Length > 80)
+            {
+                s = s.Substring(0, 80);
+            }
+
+            s = s.Replace("ş", "s"); //karakter değişimi için kullanılır
+            s = s.Replace("Ş", "S");
+            s = s.Replace("ğ", "g");
+            s = s.Replace("Ğ", "G");
+            s = s.Replace("İ", "I");
+            s = s.Replace("ı", "i");
+            s = s.Replace("ç", "c");
+            s = s.Replace("Ç", "C");
+            s = s.Replace("ö", "o");
+            s = s.Replace("Ö", "O");
+            s = s.Replace("ü", "u");
+            s = s.Replace("Ü", "U");
+            s = s.Replace("'", "");
+            s = s.Replace("\"", "");
+            Regex r = new Regex("[^a-zA-Z0-9_-]");
+            //if (r.IsMatch(s))
+            s = r.Replace(s, "-");
+            if (!string.IsNullOrEmpty(s))
+                while (s.IndexOf("--") > -1)
+                    s = s.Replace("--", "-");
+            if (s.StartsWith("-")) s = s.Substring(1);
+            if (s.EndsWith("-")) s = s.Substring(0, s.Length - 1);
+            return s;
+        }
+    }
+}
---
+++ WebProje/Areas/Admin/Controllers/TeacherController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebProje.Models;
+using WebProje.Areas;
+
+namespace WebProje.Areas.Admin.Controllers
+{
+    public class TeacherController : Controller
+    {
+        // GET: Admin/Teacher
+        public ActionResult Index()
+        {
+            using (MuratErenNarDatabaseEntities db = new MuratErenNarDatabaseEntities())
+            {
+                var model = db.Teacher.ToList();
+                return View(model);
+            }
+        }
+
+        public ActionResult TeacherUpdate()
+        {
+            using (MuratErenNarDatabaseEntities db = new MuratErenNarDatabaseEntities())
+            {
+                var model = db.Teacher.First();
+                return View(model);
+            }
+        }
+
+        public ActionResult Save(Teacher teacher)
+        {
+            using (MuratErenNarDatabaseEntities db = new MuratErenNarDatabaseEntities())
+            {
+                var updateTeacher = db.Teacher.First();
+                if (!ModelState.IsValid)
+                {
+                    return View("TeacherUpdate",teacher);
+                }
+                if (teacher.TeacherImage != null)
+                {
+                    teacher.TeacherImage = 
+                }
+            }
+        }
+
+    }
+}
